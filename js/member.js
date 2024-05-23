2f58b4829cd7cca268c8619bac8c2e8a3afb9f7a
Myoung Ha, Ji <webmaster@null0xff.com>
2024-05-23 09:16:00 +0900

    :zap: Promise 기반에서 Async Await & Promise로 구조 변경
    ERR_INSUFFICIENT_RESOURCES 우회

--- js/member.js
+++ js/member.js
@@ -1,6 +1,11 @@
-// Function to save data as JSON file
+/**
+ * Save data as JSON file
+ * @param {string} fileName - The name of the file to save.
+ * @param {Object} data - The data to save.
+ * @returns {void}
+ */
 function saveAsJSON(fileName, data) {
-    console.log("saveAsJSON(" + fileName + ", ... ) called");
+    // console.log("saveAsJSON(" + fileName + ", ... ) called");
     var fileContent = JSON.stringify(data);
     var bb = new Blob([fileContent], { type: "text/plain" });
     var a = document.createElement("a");
@@ -10,19 +15,22 @@ function saveAsJSON(fileName, data) {
     a.click();
 }
 
-// Function to get member data for a single email address
-function getMemberDataPromise(mailAddress) {
-    var formData = {
+/**
+ * Fetch member data for a single email address.
+ * @param {string} mailAddress - The email address to search for.
+ * @returns {Promise<Object>} - A promise that resolves with the member data.
+ */
+function fetchMember(mailAddress) {
+    var form = {
         searchCsTitle: mailAddress,
         searchListCnt: 100,
         pageIndex: 1
     };
-
     return new Promise((resolve, reject) => {
         jQuery.ajax({
             url: "/user/member/selectMemberList.do",
             type: "post",
-            data: formData,
+            data: form,
             dataType: "json",
             success: function (data) {
                 if (data.list.length === 0) {
@@ -34,6 +42,7 @@ function getMemberDataPromise(mailAddress) {
                         console.error(mailAddress + " not found on server");
                         resolve(null);
                     } else {
+                        console.info("found a member on server!");
                         resolve(filteredData.map(item => ({
                             csMemberSeq: item.csMemberSeq,
                             csMemberId: item.csMemberId,
@@ -52,22 +61,20 @@ function getMemberDataPromise(mailAddress) {
                 console.error(status);
                 reject(error);
             }
-        });
-    });
+        })
+    })
 }
 
-// Function to get member data for an array of email addresses with rate limiting
-async function getMemberDataArrayPromiseLimit(mailArray, limit = 1000) {
-    const promises = mailArray.map(mailAddress => getMemberDataPromise(mailAddress));
-    return limitRequestRate(promises, limit);
-}
-
-// Function to get course data for a single member data
-function getCourseDataPromise(memberData) {
+/**
+ * Fetch course data for a single member.
+ * @param {Object} memberData - The member data to search for.
+ * @returns {Promise<Object>} - A promise that resolves with the course data.
+ */
+function fetchCourse(memberData) {
     const requestData = {
-        dspLinkMenuId: "MG0086",
-        dspMenuId: "MG0085",
-        searchCsMemberSeq: memberData[0].csMemberSeq
+        dspLinkMenuId: $("#dspLinkMenuId_member").val(),
+        dspMenuId: $("#dspMenuId_member").val(),
+        searchCsMemberSeq: memberData.csMemberSeq
     };
 
     return new Promise((resolve, reject) => {
@@ -81,7 +88,7 @@ function getCourseDataPromise(memberData) {
                 jQuery("#applyRegArea").html(result);
                 var $memberForm = jQuery("#memberSearchForm_member");
                 $memberForm[0].searchListCnt_member.value = 100;
-                $memberForm[0].searchCsMemberSeq_member.value = memberData[0].csMemberSeq;
+                $memberForm[0].searchCsMemberSeq_member.value = memberData.csMemberSeq;
                 jQuery("#pageIndex_member", $memberForm).val(1);
                 resolve(new Promise((resolve, reject) => {
                     jQuery.ajax({
@@ -90,11 +97,12 @@ function getCourseDataPromise(memberData) {
                         data: $memberForm.serialize(),
                         dataType: "json",
                         success: function (data) {
+                            console.info("found course data from server!");
                             const resultData = {
-                                member: memberData[0],
+                                member: memberData,
                                 list: []
                             }
-                            resultData.member = memberData[0];
+                            resultData.member = memberData;
                             for (var i = 0; i < data.list.length; i++) {
                                 resultData.list[i] = (({ csYear, csCategoryName, csTitle, csCompletionYn, studyStartDate, studyEndDate, openStartDate, openEndDate }) => ({ csYear, csCategoryName, csTitle, csCompletionYn, studyStartDate, studyEndDate, openStartDate, openEndDate }))(data.list[i]);
                             }
@@ -107,6 +115,7 @@ function getCourseDataPromise(memberData) {
                                 jQuery.ajax(this);
                                 return;
                             } else {
+                                console.error("failed to fetch course data from server!");
                                 reject(xhr, status, error);
                             }
                         }
@@ -126,36 +135,94 @@ function getCourseDataPromise(memberData) {
     });
 }
 
-// Function to get course data for an array of member data with rate limiting
-async function getCourseDataArrayPromiseLimit(memberDataArray, limit = 1000) {
-    const promises = memberDataArray.map(memberData => getCourseDataPromise(memberData));
-    return limitRequestRate(promises, limit);
+/**
+ * Batching function for dividing work into smaller chunks.
+ * @param {Array} batch - The array of items to process.
+ * @param {Function} func - The function to apply to each item in the batch.
+ * @returns {Promise<Array>} - A promise that resolves with the results of the function applied to each item in the batch.
+ * @async
+ */
+async function runBatch(batch, func) {
+    const promises = [];
+    for (var i = 0; i < batch.length; i++) {
+        promises.push(func(batch[i]));
+    }
+    return await Promise.all(promises);
 }
 
-// Function to limit the rate of AJAX requests
-async function limitRequestRate(promiseArray, limit) {
-    const result = [];
-    while (promiseArray.length) {
-        const batch = promiseArray.splice(0, limit);
-        await Promise.all(batch)
-            .then(values => result.push(...values))
-            .catch(error => console.error("Error in batch request:", error));
+/**
+ * Fetch member data for a list of email addresses.
+ * @param {Array<string>} mailArray - The list of email addresses to search for.
+ * @param {number} [limit=6] - The number of email addresses to search for in a single batch.
+ * @returns {Promise<Array<Object>>} - A promise that resolves with an array of member data.
+ * @async
+ */
+async function fetch(mailArray, limit = 6) {
+    var batch = [];
+    while (mailArray.length > 0) {
+        batch.push(mailArray.splice(0, limit));
     }
+
+    var rawMembers = [];
+    for (var i = 0; i < batch.length; i++) {
+        var batchData = await runBatch(batch[i], fetchMember);
+        for (var j = 0; j < batchData.length; j++) {
+            rawMembers.push(batchData[j]);
+        }
+    }
+
+    var members = [];
+    for (var i = 0; i < rawMembers.length; i++) {
+        if (rawMembers[i]) {
+            for (var j = 0; j < rawMembers[i].length; j++) {
+                members.push(rawMembers[i][j]);
+            }
+        }
+    }
+
+    batch = [];
+    while (members.length > 0) {
+        batch.push(members.splice(0, limit));
+    }
+
+    var result = [];
+    for (var i = 0; i < batch.length; i++) {
+        var batchData = await runBatch(batch[i], fetchCourse);
+        for (var j = 0; j < batchData.length; j++) {
+            result.push(batchData[j]);
+        }
+    }
+
     return result;
 }
 
-// Function to search and save member data
-function searchMembers() {
+/**
+ * Search members by mail addresses.
+ * @param {number} limit - The limit of members to search for.
+ * @returns {Promise<Object>} - A promise that resolves with the member data.
+ * @async
+ */
+async function searchMembers(limit = 6) {
     var input = prompt("Input Mail Address(es)");
     var mailArray = input.trim().replace(/\r/g, "").split("\n");
-    getMemberDataArrayPromiseLimit(mailArray)
-        .then((memberDataArray) => {
-            getCourseDataArrayPromiseLimit(memberDataArray)
-                .then((courseDataArray) => {
-                    saveAsJSON("", courseDataArray);
-                });
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
+
+    console.log("Searching members...")
+    const begin = new Date();
+
+    var batch = [];
+    while (mailArray.length > 0) {
+        batch.push(mailArray.splice(0, limit * 100));
+    }
+
+    const result = [];
+    for (var i = 0; i < batch.length; i++) {
+        console.log(`Batch ${i + 1} of ${batch.length}...`);
+        fetched = await fetch(batch[i], limit);
+        for (var j = 0; j < fetched.length; j++) {
+            result.push(fetched[j]);
+        }
+    }
+
+    console.log("Search completed in " + (new Date() - begin) / 1000 + " seconds.");
+    saveAsJSON("", result);
 }
