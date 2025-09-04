946b323c41567b2e01c3b538fa57962a359b5898
Myoung Ha, Ji <webmaster@null0xff.com>
2025-09-04 10:32:27 +0900

    🩹 누락된 코드 추가

--- src/scripts/course.js
+++ src/scripts/course.js
@@ -24,6 +24,244 @@ let circuitBreakerState = {
 };
 
 /**
+ * Represents a completion.
+ * @class
+ * @property {number} csMemberSeq - The member sequence number.
+ * @property {string} csMemberId - The member username.
+ * @property {string} csMemberName - The member name.
+ * @property {string} cxMemberEmail - The member email.
+ * @property {string} csApplyStatusCd - The application status code.
+ * @property {string} csCompletionYn - The completion status.
+ * @property {Date|null} cxCompletionDate - The completion date.
+ * @constructor
+ * @param {number} csMemberSeq - The member sequence number.
+ * @param {string} csMemberId - The member username.
+ * @param {string} csMemberName - The member name.
+ * @param {string} cxMemberEmail - The member email.
+ * @param {string} csApplyStatusCd - The application status code.
+ * @param {string} csStudyStartDate - The study start date.
+ * @param {string} csCompletionYn - The completion status.
+ * @param {string} cxCompletionDate - The completion date.
+ * @returns {Completion} - The completion object.
+ */
+class Completion {
+  constructor(csMemberSeq, csMemberId, csMemberName, cxMemberEmail,
+    csApplyStatusCd, csStudyStartDate, csCompletionYn, cxCompletionDate) {
+    this.csMemberSeq = csMemberSeq;
+    this.csMemberId = csMemberId;
+    this.csMemberName = csMemberName;
+    this.cxMemberEmail = cxMemberEmail;
+    this.csApplyStatusCd = csApplyStatusCd;
+    this.csStudyStartDate = csStudyStartDate;
+    this.csCompletionYn = csCompletionYn;
+    this.cxCompletionDate = this.formatCompletionDate(cxCompletionDate);
+  }
+
+  /**
+   * Formats the completion date.
+   *
+   * Original Date: yyyyMMddHHmmss
+   * Formatted Date: yyyy-MM-dd
+   *
+   * Be careful that original date value is offset with local timezone.
+   * Get the timezone offset and calculate actual UTC time.
+   *
+   * @param cxCompletionDate
+   * @returns {Date|null}
+   */
+  formatCompletionDate(cxCompletionDate) {
+    if (cxCompletionDate === null) {
+      return null;
+    }
+
+    // Get the timezone offset.
+    const timezoneOffset = new Date().getTimezoneOffset() * 60000;
+
+    // Get the year, month, day, hour, minute, and second.
+    const year = cxCompletionDate.substring(0, 4);
+    const month = cxCompletionDate.substring(4, 6);
+    const day = cxCompletionDate.substring(6, 8);
+    const hour = cxCompletionDate.substring(8, 10);
+    const minute = cxCompletionDate.substring(10, 12);
+    const second = cxCompletionDate.substring(12, 14);
+
+    // Return the formatted date.
+    return new Date(
+      Date.UTC(year, month - 1, day, hour, minute, second) + timezoneOffset);
+  }
+}
+
+/**
+ * Represents a completion request.
+ * @class
+ * @constructor
+ * @param {number} courseActiveSeq - The course active sequence number.
+ * @param {number} count - The number of search results to return.
+ * @param {boolean} completion - The completion status.
+ * @returns {CompletionRequest} - The completion request object.
+ */
+class CompletionRequest {
+  static MENU_ID = 'MG0036';
+  static Order = {
+    MEMBER_ASC: 1, // Member Ascending (회원 오름차순)
+    MEMBER_DESC: -1, // Member Descending (회원 내림차순)
+    END_DATE_ASC: 4, // End Date Ascending (종료일 오름차순)
+    END_DATE_DESC: -4 // End Date Descending (종료일 내림차순)
+  }
+
+  constructor(courseActiveSeq = 0, count = 10, completion = '') {
+    this.pageIndex = 1;
+    this.searchListCnt = count;
+    this.orderBy = this.constructor.Order.MEMBER_ASC;
+    this.dspLinkMenuId = this.constructor.MENU_ID;
+    this.dspMenuId = this.constructor.MENU_ID;
+    this.searchCsCourseActiveSeq = courseActiveSeq;
+    this.searchCsCompletionYn = completion;
+    this.searchCsMemberName = '';
+    this.searchCsMemberId = '';
+  }
+}
+
+/**
+ * Represents a application request.
+ * @class
+ * @constructor
+ * @param {number} courseActiveSeq - The course active sequence number.
+ * @param {number} courseMasterSeq - The course master sequence number.
+ * @param {number} count - The number of search results to return.
+ * @returns {ApplicationRequest} - The completion request object.
+ */
+class ApplicationRequest {
+  static MENU_ID = 'MG0028';
+
+  constructor(courseActiveSeq = 0, courseMasterSeq = 0, count = 10) {
+    this.pageIndex = 1;
+    this.searchListCnt = count;
+    this.dspLinkMenuId = this.constructor.MENU_ID;
+    this.dspMenuId = this.constructor.MENU_ID;
+    this.searchCsCourseActiveSeq = courseActiveSeq;
+    this.searchCsCourseMasterSeq = courseMasterSeq;
+    // TODO: Implement following search criteria.
+    this.searchCsApplyStatusCd = '';
+    this.searchCxDivisionCd = '';
+    this.searchCsMemberId = '';
+    this.searchCsMemberName = '';
+  }
+}
+
+/**
+ * Represents a course.
+ * @class
+ * @constructor
+ * @param {number} csCourseActiveSeq - The course active sequence number.
+ * @param {number} csCourseMasterSeq - The course master sequence number.
+ * @param {string} csTitle - The course title.
+ * @param {string} csStatusCd - The course status code.
+ * @param {string} csCourseTypeCd - The course type code.
+ * @param {number} csYear - The course year.
+ * @param {string} csApplyStartDate - The course application start date.
+ * @param {string} csApplyEndDate - The course application end date.
+ * @param {string} csStudyStartDate - The course study start date.
+ * @param {string} csStudyEndDate - The course study end date.
+ * @param {string} csOpenStartDate - The course open start date.
+ * @param {string} csOpenEndDate - The course open end date.
+ * @param {number} csCmplTime - The course completion time.
+ * @param {string} csTitlePath - The course title path.
+ * @param {Completion[]} csCmplList - The course completion list.
+ * @returns {Course} - The course object.
+ */
+class Course {
+  static Status = {
+    ACTIVE: 'active', INACTIVE: 'inactive'
+  }
+  static Type = {
+    ALWAYS: 'always', PERIOD: 'period'
+  }
+  static Category = {
+    INTRO: '입문',
+    ADVANCED: '심화',
+    GENERAL: '일반',
+    CUSTOMIZED: '맞춤형',
+    FOREIGN: '외국어'
+  }
+
+  constructor(csCourseActiveSeq, csCourseMasterSeq, csTitle, csStatusCd,
+    csCourseTypeCd, csYear, csApplyStartDate, csApplyEndDate,
+    csStudyStartDate, csStudyEndDate, csOpenStartDate, csOpenEndDate,
+    csCmplTime, csTitlePath, csCmplList = []) {
+    this.csCourseActiveSeq = csCourseActiveSeq; // Class Unique Identifier
+    this.csCourseMasterSeq = csCourseMasterSeq; // Class Creation Group, Master Identifier
+    this.csTitle = csTitle;
+    this.csStatusCd = csStatusCd; // Status
+    this.csCourseTypeCd = csCourseTypeCd; // Type
+    this.csYear = csYear;
+    this.csApplyStartDate = csApplyStartDate;
+    this.csApplyEndDate = csApplyEndDate;
+    this.csStudyStartDate = csStudyStartDate;
+    this.csStudyEndDate = csStudyEndDate;
+    this.csOpenStartDate = csOpenStartDate;
+    this.csOpenEndDate = csOpenEndDate;
+    this.csCmplTime = csCmplTime;
+    this.csTitlePath = csTitlePath; // Category
+    this.csCmplList = csCmplList; // Completion List
+  }
+}
+
+/**
+ * Represents a course request.
+ * @class
+ * @constructor
+ * @param {string} search - The search criteria.
+ * @param {number} count - The number of search results to return.
+ * @param {number} year - The year to search for.
+ * @returns {CourseRequest} - The course request object.
+ */
+class CourseRequest {
+  static Order = {
+    TITLE_ASC: 1, // Title Ascending (과정명 오름차순)
+    TITLE_DESC: -1, // Title Descending (과정명 내림차순)
+    CATEGORY_ASC: 2, // Category Ascending (과정분류 오름차순)
+    CATEGORY_DESC: -2, // Category Descending (과정분류 내림차순)
+    STATUS_ASC: 3, // Status Ascending (운영상태 오름차순)
+    STATUS_DESC: -3, // Status Descending (운영상태 내림차순)
+    DATE_ASC: 4, // Date Ascending (등록일 오름차순)
+    DATE_DESC: -4 // Date Descending (등록일 내림차순)
+  }
+  static Category = {
+    ALL: '', // All (전체)
+    INTRO: '1', // Introduction (입문)
+    ADVANCED: '2', // Advanced (심화)
+    GENERAL: '3', // General (일반)
+    CUSTOMIZED: '14', // Customized (맞춤형)
+    FOREIGN: '16' // Foreign (외국어)
+  }
+  static Type = {
+    ALL: '', // All (전체)
+    ALWAYS: 'always', // Always (상시제)
+    PERIOD: 'period' // Period (기수제)
+  }
+  static Status = {
+    ALL: '', // All (전체)
+    ACTIVE: 'active', // Active (활성)
+    INACTIVE: 'inactive' // Inactive (비활성)
+  }
+
+  constructor(count = 10, search = '', year = '') {
+    this.pageIndex = 1;
+    this.searchListCnt = count;
+    this.orderBy = this.constructor.Order.DATE_DESC;
+    this.dspLinkMenuId = 'MG0027';
+    this.dspMenuId = 'MG0027';
+    this.searchCsCategorySeq = this.constructor.Category.ALL;
+    this.searchCsSubjectCode = ''; // Subject Type (교육분야)
+    this.searchCsYear = year;
+    this.searchCsCourseTypeCd = this.constructor.Type.ALL;
+    this.searchCsStatusCd = this.constructor.Status.ALL;
+    this.searchCsTitle = search;
+  }
+}
+
+/**
  * Sleep utility function
  */
 function sleep(ms) {
